8bc2c1734261713c5786bd56577e77c803807952
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2025-07-16 00:07:08 -0700

    completed 4.1~4.2

--- react_challenge_4.1.js
+++ react_challenge_4.1.js
@@ -1,1 +1,42 @@
-//Passing Data Deeply With Context #1
+//Referencing Values with Refs
+import { useState } from 'react';
+import { useRef } from 'react';
+
+export default function Chat() {
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  let timeoutID = useRef(0);
+
+  function handleSend() {
+    setIsSending(true);
+    timeoutID.current = setTimeout(() => {
+      alert('Sent!');
+      setIsSending(false);
+    }, 3000);
+  }
+
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID.current);
+  }
+
+  return (
+    <>
+      <input
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button
+        disabled={isSending}
+        onClick={handleSend}>
+        {isSending ? 'Sending...' : 'Send'}
+      </button>
+      {isSending &&
+        <button onClick={handleUndo}>
+          Undo
+        </button>
+      }
+    </>
+  );
+}
--- react_challenge_4.2.js
+++ react_challenge_4.2.js
@@ -1,1 +1,18 @@
-//Passing Data Deeply With Context #1
+//Synchronizing With Effects #1
+import { useEffect, useRef } from 'react';
+
+export default function MyInput({ value, onChange }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
+  return (
+    <input
+      ref={ref}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
